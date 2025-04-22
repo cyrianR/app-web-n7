1a94cf50f0bd7caff3f3122519e76a5b92cc9221
Nino Rottier <nino.rottier@free.fr>
2025-04-22 15:39:39 +0200

    cleaning imports (c'est le dernier promis)

--- spring-boot-api/src/main/java/fr/n7/spring_boot_api/controller/VoteController.java
+++ spring-boot-api/src/main/java/fr/n7/spring_boot_api/controller/VoteController.java
@@ -1,6 +1,5 @@
 package fr.n7.spring_boot_api.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.n7.spring_boot_api.model.Vote;
