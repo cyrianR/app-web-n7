86bf62b6ac1648b6bd9079dbc6d94e27f08a050d
Maruwuwa <marwasan03@gmail.com>
2025-04-22 15:13:33 +0200

    Création des fichiers

---
+++ spring-boot-api/src/main/java/fr/n7/spring_boot_api/model/File.java
@@ -0,0 +1,58 @@
+package fr.n7.spring_boot_api.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "files")
+public class File {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    private String name;
+
+    // Type du fichier : jpeg, txt...
+    private String type;
+
+    // Permet d'indiquer que l'attribut doit être stocké sous forme de grande donnée
+    @Lob
+    private byte[] data;
+
+    public File() {
+    }
+
+    public File(String name, String type, byte[] data) {
+        this.name = name;
+        this.type = type;
+        this.data = data;
+    }
+    
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+}
---
+++ spring-boot-api/src/main/java/fr/n7/spring_boot_api/service/FileStorageService.java
@@ -0,0 +1,34 @@
+package fr.n7.spring_boot_api.service;
+
+import java.io.IOException;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import fr.n7.spring_boot_api.model.File;
+import fr.n7.spring_boot_api.repository.FileRepository;
+
+@Service
+public class FileStorageService {
+
+  @Autowired
+  private FileRepository fileRepository;
+
+  public File store(MultipartFile file) throws IOException {
+    String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+    File File = new File(fileName, file.getContentType(), file.getBytes());
+
+    return fileRepository.save(File);
+  }
+
+  public File getFile(String id) {
+    return fileRepository.findById(id).get();
+  }
+  
+  public Stream<File> getAllFiles() {
+    return fileRepository.findAll().stream();
+  }
+}
