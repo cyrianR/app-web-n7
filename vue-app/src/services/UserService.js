2f96d85e32c6832e8b800b76a5224d5e9e785a06
cyrianR <ragotcyrian@protonmail.com>
2025-05-02 21:20:27 +0200

    feat(service): implement CRUD operations for user management in UserService

--- vue-app/src/services/UserService.js
+++ vue-app/src/services/UserService.js
@@ -1,6 +1,32 @@
 import http from "../http-common";
-import authHeader from "./auth-header";
 
 class UserService {
+  getAll(username = null) {
+    if (username) {
+      return http.get(`/users?username=${username}`);
+    }
+    return http.get("/users");
+  }
 
+  getById(id) {
+    return http.get(`/user/${id}`);
+  }
+
+  create(user) {
+    return http.post("/user", user);
+  }
+
+  update(id, user) {
+    return http.put(`/user/${id}`, user);
+  }
+
+  updateRoles(id, roles) {
+    return http.put(`/user/role/${id}`, roles);
+  }
+
+  delete(id) {
+    return http.delete(`/user/${id}`);
+  }
 }
+
+export default new UserService();
