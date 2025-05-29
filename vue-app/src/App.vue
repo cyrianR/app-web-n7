a5a5e1791a985620cbb493959c9eab0bd23401ca
cyrianR <ragotcyrian@protonmail.com>
2025-05-29 17:23:19 +0200

    trying to better front App.vue

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -16,7 +16,6 @@ export default {
       if (message.userId === this.$store.state.auth.user.id) {
         // Update roles
         this.updateRoles(message.roles);
-        alert("Your roles have been updated!");
       }
     });
   },
@@ -39,44 +38,71 @@ export default {
         <router-link to="/" class="navbar-brand">
           <img src="/img/logo_clean_saisons_rond.png" width="50" height="50" alt="">
         </router-link>
-        <div class="flex-row justify-content-end" id="navbarNav">
+        <!--  <div class="flex-row justify-content-end" id="navbarNav">-->
             <ul class="navbar-nav flex-row gap-4">
-            <li class="nav-item">
-              <router-link to="/" class="nav-link">Accueil</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="" class="nav-link">Cuisine</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="" class="nav-link">Karaoke</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="" class="nav-link">Cours</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="" class="nav-link">Projection</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="/photos" class="nav-link">Photos</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="/agenda" class="nav-link">Agenda</router-link>
-            </li>
-            <div class="d-flex flex-column justify-content-center">
-              <li v-if="isLoggedIn" class="nav-item">
-                <router-link to="/profile" class="btn btn-primary">Compte</router-link>
+              <li class="nav-item">
+                <router-link to="/" class="nav-link">Accueil</router-link>
               </li>
-              <div v-else class="d-flex flex-row gap-3">
-                <li class="nav-item">
-                  <router-link to="/register" class="btn btn-primary">S'inscrire</router-link>
+              <li class="nav-item">
+                <router-link to="" class="nav-link">Cuisine</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="" class="nav-link">Karaoke</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="" class="nav-link">Cours</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="" class="nav-link">Projection</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/photos" class="nav-link">Photos</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/agenda" class="nav-link">Agenda</router-link>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle "
+                  href="#"
+                  id="userDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <i class="bi bi-person-fill"></i>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+                  <li v-if="isLoggedIn">
+                    <router-link to="/profile" class="dropdown-item">Mon profil</router-link>
+                  </li>
+                  <li v-if="isLoggedIn">
+                    <button @click="logout" class="dropdown-item">Se déconnecter</button>
+                  </li>
+                  <li v-else>
+                    <router-link to="/register" class="dropdown-item">S'inscrire</router-link>
+                  </li>
+                  <li v-else>
+                    <router-link to="/login" class="dropdown-item">Se connecter</router-link>
+                  </li>
+                </ul>
+              </li>
+              
+              <!-- <div class="d-flex flex-column justify-content-center">
+                <li v-if="isLoggedIn" class="nav-item">
+                  <router-link to="/profile" class="btn btn-primary">Compte</router-link>
                 </li>
-                <li class="nav-item">
-                  <router-link to="/login" class="btn btn-primary">Se connecter</router-link>
-                </li>
-              </div>
-            </div>
+                <div v-else class="d-flex flex-row gap-3">
+                  <li class="nav-item">
+                    <router-link to="/register" class="btn btn-primary">S'inscrire</router-link>
+                  </li>
+                  <li class="nav-item">
+                    <router-link to="/login" class="btn btn-primary">Se connecter</router-link>
+                  </li>
+                </div>
+              </div> -->
             </ul>
-        </div>
+        <!--  </div>-->
       </div>
     </nav>
 
@@ -108,6 +134,32 @@ export default {
 </template>
 
 <style scoped>
+.navbar {
+  overflow: visible; /* Prevent content overflow */
+}
+
+.navbar > .container-fluid {
+  height: 60px;
+}
+
+
+
+.dropdown-menu {
+  position: absolute; /* Position the dropdown menu relative to the dropdown button */
+  top: calc(100% + 5px); /* Position the dropdown slightly below the button */
+  left: auto; /* Align dropdown menu correctly */
+  right: 0; /* Align dropdown menu to the right of the button */
+  z-index: 1050; /* Ensure the dropdown is above other elements */
+  min-width: 200px; /* Set a minimum width for the dropdown menu */
+  width: auto; /* Allow the dropdown menu to expand based on content */
+}
+
+.navbar-nav {
+  height: 40px; /* Match the height of the navbar */
+  display: flex;
+  /* align-items: center; /* Center items vertically */
+}
+
 .navbar-nav > li > a {
   color: black;
   font-weight: 500;
